279fa141ffd4525b685dc63de1a6d7ae7ba03bb2
Joe Chek <jmchek@gmail.com>
2022-12-21 15:17:40 -0800

    add calc functionality with multiple pairs w/ operators

--- calculator.js
+++ calculator.js
@@ -39,48 +39,18 @@ function calc() {
     let secNum = "";
     let operator;
     let firstPairPicked = false;
+    let equalsHit = false;
     let storage = 0;
 
-    //add listener for operator here
     operatorButton.forEach((button) => {
         button.addEventListener('click', () => {
             firstPairPicked = true;
-            
+            if (firstNum && firstPairPicked && !secNum && !equalsHit) {
+                storage = parseInt(firstNum);
+            }
+
             switch(button.id) {
-                case 'add': {
-                    operator = add;
-                    
-                    
-                    // storage = operate(operator, firstNum, parseInt(secNum));
-                    // display(storage);
-                    // firstNum = "";
-                    // secNum = "";
-                    // console.log(storage);
-                    if (firstNum && firstPairPicked && !secNum) {
-                        storage = parseInt(firstNum);
-                    }
-
-                    if (secNum != "") {
-                        console.log(storage);
-                        storage = operate(operator, parseInt(storage), parseInt(secNum));
-                        display(storage);
-                        console.log(storage);
-                        secNum = "";
-                    }
-
-                    
-
-                    // if(firstPairPicked) {
-                    //     storage = operate(operator, parseInt(firstNum), parseInt(secNum));
-                    //     display(storage);
-                    //     firstNum = storage;
-                    //     secNum = "";
-                    // }
-
-                    // firstPairPicked = !firstPairPicked;
-
-                    // console.log(storage);
-                }
+                case 'add': operator = add;
                 break;
                 case 'subtract': operator = subtract;
                 break;
@@ -90,8 +60,20 @@ function calc() {
                 break;
             }  
 
-            // chosenOperator(parseInt(firstNum), numButton, operator, operatorButton); 
-            // firstNum = "";
+            if (secNum != "" && !equalsHit) {
+                storage = operate(operator, parseInt(storage), parseInt(secNum));
+                display(storage);
+                secNum = "";
+            } 
+            
+            if (secNum && equalsHit) {
+                storage = operate(operator, parseInt(storage), parseInt(secNum));
+                display(storage);
+                secNum = "";
+            }
+
+            console.log(storage);
+
         });
     });
     
@@ -101,87 +83,22 @@ function calc() {
                 if (!firstPairPicked){
                 firstNum += button.innerText;
                 display(firstNum);
-                }
-
-                if(firstPairPicked) {
+                } else {
                 secNum += button.innerText;
                 display(secNum);
             }
         });
     });
 
-    // numButton.forEach((button) => {
-    //     button.addEventListener('click', () => {
-    //             secNum += button.innerText;
-    //             display(secNum); 
-    //     });
-    // });
-
     equalsButton.addEventListener('click', () => {
-        // display(operate(operator, firstNum, parseInt(secNum)));
-        storage = operate(operator, parseInt(firstNum), parseInt(secNum));
+        equalsHit = true;
+        storage = operate(operator, parseInt(storage), parseInt(secNum));
         display(storage);
+        secNum = "";
+
+        console.log(storage);
     });
 
 }
 
-//function to store operator selected
-// function chosenOperator(firstNum, numButton, operator, operatorButton) {
-//     const equalsButton = document.querySelector('#equals');
-//     this.numButton = numButton;
-//     this.firstNum = firstNum;
-//     this.operator = operator;
-//     let secNum = "";
-//     let storage = parseInt(firstNum);
-//     let equalsHit = false;
-
-//     operatorButton.forEach((button) => {
-//         button.addEventListener('click', () => {
-//             switch(button.id) {
-//                 case 'add': {
-//                     storage = operate(operator, firstNum, parseInt(secNum));
-//                     display(storage);
-//                     firstNum = "";
-//                     secNum = "";
-//                     console.log(storage);
-//                     // while(!equalsHit){
-
-//                     // }
-//                 }
-//                 break;
-//                 case 'subtract': operator = subtract;
-//                 break;
-//                 case 'multiply': operator = multiply;
-//                 break;
-//                 case 'divide': operator = divide;
-//                 break;
-//             }  
-
-//         });
-//     });
-
-//     numButton.forEach((button) => {
-//         button.addEventListener('click', () => {
-//                 secNum += button.innerText;
-//                 display(secNum); 
-//         });
-//     });
-
-//     equalsButton.addEventListener('click', () => {
-//         // display(operate(operator, firstNum, parseInt(secNum)));
-//         storage = operate(operator, firstNum, parseInt(secNum));
-//         display(storage);
-//     });
-
-    
-// }
-
 calc();
-
-// figuring out the logic of chaining multiple pairs
-
-// user inputs the first number of pair
-// once operator is chosen, the firstNum is sent to chosenOperator() function to be stored
-// then the user selects the second number
-// when the operator is hit again, the firstNum, secNum, and operator are calculated and the answer is displayed and stored
-// now we need to use the stored number and operate with the second of pair
